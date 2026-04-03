83b2b7765ebb59bf9e3d211065e76172476ba88c
Aiman Hakim <aiman.azahari@tmrnd.com.my>
2026-04-03 16:03:07 +0800

    fix: update dashboard title from 'LaluanMY' to 'Laluan'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
     {
         path: 'dashboard',
         component: DashboardComponent,
-        title: 'LaluanMY'
+        title: 'Laluan'
     },
     {
         path: '**',
